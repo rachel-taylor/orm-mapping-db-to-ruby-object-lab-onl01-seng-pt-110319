09a9e0f95d5616c2dfddd98b2e26fa08e0af95a3
Rachel T <rachmaybe@gmail.com>
2020-02-16 02:10:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,7 +57,7 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  def self.count_all_students_in_grade_9
+  def self.count_all_students_in_grade_X
     sql = <<-SQL
        SELECT COUNT(grade = 9) FROM students
     SQL
