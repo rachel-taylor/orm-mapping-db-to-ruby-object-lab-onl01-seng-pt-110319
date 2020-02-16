4155d6595ec88fda4d462315a771cb4818c3ec91
Rachel T <rachmaybe@gmail.com>
2020-02-16 02:08:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,42 +10,23 @@ class Student
   end
 
   def self.all
-    sql = "SELECT * FROM students"
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-  end
-
-  def self.find_by_name(name)
-    sql = <<-SQL
-    # return a new instance of the Student class	    SELECT * 
-    FROM students
-    WHERE name = ?
-    LIMIT 1
-    SQL
-    DB[:conn].execute(sql,name).map do |row|
-      self.new_from_db(row)
-    end.first
-  end
-
-  def self.students_below_12th_grade
     sql = <<-SQL
     SELECT * 
-    FROM students 
-    WHERE students.grade < 12
+    FROM students
     SQL
-    DB[:conn].execute(sql).collect do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
   end
 
-  def self.first_student_in_grade_10
+  def self.find_by_name(name)
     sql = <<-SQL
     SELECT * 
-    FROM students
-    WHERE students.grade = 10
+    FROM students 
+    WHERE name = ? 
     LIMIT 1
     SQL
-    DB[:conn].execute(sql).collect do |row|
+    DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end.first
   end
@@ -75,4 +56,41 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT * 
+    FROM students 
+    WHERE grade < 12
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 10
+    ORDER BY id
+    LIMIT 1
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+    SELECT COUNT(grade)
+    FROM students
+    WHERE grade = 9
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+
 end
